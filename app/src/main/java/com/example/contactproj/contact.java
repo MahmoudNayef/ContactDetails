e69674dfd524087ffdc9c3b5ee3271f31846d1c5
Your Name <you@example.com>
2021-03-31 15:20:51 +0300

    First Commit

---
+++ app/src/main/java/com/example/contactproj/contact.java
@@ -0,0 +1,70 @@
+package com.example.contactproj;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class contact extends RecyclerView.Adapter<contact.ViewHolder> {
+
+    private List<model> mData;
+    private LayoutInflater mInflater;
+
+
+    // data is passed into the constructora.fm
+    contact(Context context, List<model> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.items, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        holder.name.setText(mData.get(position).getName());
+        holder.phone.setText(mData.get(position).getPhone());
+        holder.address.setText(mData.get(position).getAddress());
+
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder  {
+        TextView name;
+        TextView phone;
+        TextView address;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.rvName);
+            phone = itemView.findViewById(R.id.rvPhone);
+            address = itemView.findViewById(R.id.rvAddress);
+
+
+        }
+
+
+    }
+
+
+
+
+}
